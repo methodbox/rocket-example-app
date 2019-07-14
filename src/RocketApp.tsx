f3adcf91830c0a203dbbad4147c21334669e8462
Chris Shaffer <chris@methodbox.io>
2019-07-14 18:33:55 -0500

    boilerplate for example ts app using SpaceX API

---
+++ src/RocketApp.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './styles.css';
+
+interface Props {}
+
+interface State {
+  welcomeMessage: string;
+  rocketName: string | null;
+}
+export default class RocketApp extends React.Component<Props, State> {
+  state: State = {
+    welcomeMessage: 'Random SpaceX Rocket',
+    rocketName: null,
+  };
+
+  _getNewRocket() {
+    let flightNumber = Math.floor(Math.random() * 81);
+    fetch(`https://api.spacexdata.com/v3/launches/${flightNumber}`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ rocketName: json.rocket.rocket_id });
+        console.log(json);
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.welcomeMessage}</h1>
+        <h3>{this.state.rocketName}</h3>
+        <button onClick={() => this._getNewRocket()}>Random Rocket</button>
+      </div>
+    );
+  }
+}
